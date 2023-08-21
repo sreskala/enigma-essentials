cb6605b24ef693f58455e7c93d7e4a00530e6484
Samuel Reskala <sreskala@Samuels-Air.attlocal.net>
2023-08-21 16:20:50 -0400

    adding custom errors

---
+++ src/errors/index.ts
@@ -0,0 +1,6 @@
+export * from './bad-request-error';
+export * from './request-validation-error';
+export * from './database-connection-error';
+export * from './not-authorized-error';
+export * from './not-found-error';
+export * from './custom-error';
